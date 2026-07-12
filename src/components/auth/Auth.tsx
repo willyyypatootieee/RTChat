ebd897523a88cd1cb70f41a248e09cae9c7852db
willyyypatootieee <blueplayz26@gmail.com>
2026-07-12 10:21:23 +0700

    feat: Implement Auth component for user authentication

---
+++ src/components/auth/Auth.tsx
@@ -0,0 +1,29 @@
+import { Button, Stack, TextField } from '@mui/material';
+import { useState } from 'react';
+
+
+interface AuthProps {
+  submitLabel: string
+  onSubmit: (credentials: {
+    email: string, 
+    password: string
+  }) => Promise<void>;
+}
+
+const Auth = ({submitLabel, onSubmit}: AuthProps) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  return (
+    <Stack spacing={3} sx={{ width: '100%', maxWidth: { xs: '100%', sm: '400px' }, margin: '0 auto', marginTop: '2rem' }}>
+      <TextField type="email" label="Email" variant="outlined" value={email} onChange={(event) => setEmail(event.target.value)} />
+      <TextField type="password" label="Password" variant="outlined" value={password} onChange={(event) => setPassword(event.target.value)} />
+      <Button variant="contained" onClick={() => onSubmit({ email, password })}>
+        {submitLabel}
+      </Button>
+    </Stack>
+  )
+};
+
+export default Auth
+
