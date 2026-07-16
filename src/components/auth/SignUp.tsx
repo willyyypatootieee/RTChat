e0a730015a24974dd51f44b5af290439f81efee9
willyyypatootieee <blueplayz26@gmail.com>
2026-07-17 02:51:51 +0700

    feat: Implement SignUp component with user creation mutation and navigation to Login

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,6 +1,30 @@
 import Auth from './Auth';
+import { Link } from "react-router-dom";
+import { Link as MUILink } from "@mui/material";
+import {useMutation} from "@apollo/client/react";
+import {CREATE_USER} from "../../hooks/useCreateUser";
+
 const SignUp = () => {
-    return <Auth submitLabel="SignUp" onSubmit={async() => {}}/>
+
+    const  [createUser] = useMutation(CREATE_USER);
+    return (
+        <>
+            <Auth submitLabel="Sign Up" onSubmit={async ({email, password}) => {
+                await createUser({
+                    variables: {
+                        createUserInput: {
+                            email,
+                            password
+                        }
+                    }
+                })
+            }}>
+                <Link to={"/login"}>
+                <MUILink>Login</MUILink>
+                </Link>
+            </Auth>
+        </>
+    );
 };
 
 export default SignUp;
