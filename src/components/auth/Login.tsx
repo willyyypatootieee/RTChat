ff381eb65634a46401db674c01033ef58587fee5
willyyypatootieee <blueplayz26@gmail.com>
2026-07-17 02:37:29 +0700

    feat: Enhance Login component to include SignUp link

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,7 +1,18 @@
 import Auth from './Auth';
+import { Link } from "react-router-dom";
+import { Link as MUILink } from "@mui/material";
+
 
 const Login = () => {
-    return <Auth submitLabel="Login" onSubmit={async() => {}}/>
+    return (
+        <>
+            <Auth submitLabel="Login" onSubmit={async () => {}}>
+                <Link to={"/signUp"}>
+                <MUILink>SignUp</MUILink>
+                </Link>
+            </Auth>
+        </>
+    );
 };
 
 export default Login;
